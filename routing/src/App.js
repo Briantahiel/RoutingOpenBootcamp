4af514bf3ac63687802e609889e9b5b8790292ee
BrianTahiel <briang.2012@hotmail.com>
2023-03-17 01:02:05 -0300

    last commit

--- routing/src/App.js
+++ routing/src/App.js
@@ -1,21 +1,24 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './Components/Forms/Login';
 import Register from './Components/Forms/Register';
+import NotFound from './Components/NotFound';
 import Tasks from './Components/Tasks/Tasks';
 
 function App() {
-  const logged = false;
+  const [isLogged, setIsLogged] = useState(false);
   return (
     <Router>
-      <Routes>
-        <Route exact path='/login' element={<Login isLogged={logged} />}/>
-        <Route exact path='/register' element={<Register />}/>
-        <Route exact path='/tasks' element={<Tasks/>}/>
-        <Route exact path='/' element={logged ? <Navigate from='/' to='/tasks' /> : <Navigate from='/' to="/login" />} />
+        <Routes>
+        <Route exact path='/login' element={<Login setIsLogged={setIsLogged} />} />
+        <Route exact path='/register' element={<Register />} />
+        <Route exact path='/tasks' element={isLogged ? <Tasks isLogged={isLogged} /> : <Navigate from='/tasks' to='/login' />} />
+        <Route exact path='/' element={isLogged ? <Tasks isLogged={isLogged} /> : <Navigate to="/login" />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </Router>
   );
 }
 
 export default App;
+
--- routing/src/Components/Tasks/Tasks.jsx
+++ routing/src/Components/Tasks/Tasks.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
-const Tasks = () => {
+const Tasks = ({ isLogged }) => {
+  const navigate = useNavigate();
+
+  if (!isLogged) {
+    navigate('/login');
+    return null;
+  }
+  const goBack = () => {
+    navigate(-1);
+}
+  
   return (
     <div>
-        <h2>My Tasks</h2>
+      <h2>Tasks</h2>
+      <p>List of tasks.</p>
+      <button onClick={goBack}>Go Back</button>
     </div>
-  )
-}
+  );
+};
 
-export default Tasks
+export default Tasks;
+
+
+
